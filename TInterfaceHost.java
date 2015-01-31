60e974a7692823229a53c9d0b5f1e408bbbf3dbc
timoffex <timoffex@gmail.com>
2015-01-31 15:18:40 -0800

    Host interface template.
    Implement this class and write the getClients() method to return all
    connected clients.

---
+++ TInterfaceHost.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public abstract class TInterfaceHost {
+	public final void broadcastStart() {
+		List<TClient> clients = getClients();
+
+		long time = Date.getTime();
+
+		for (TClient t : clients) {
+			t.start(time);
+		}
+	}
+
+	public final void broadcastStopImmediate() {
+		List<TClient> clients = getClients():
+
+		for (TClient t : clients) {
+			t.stopNow();
+		}
+	}
+
+	public final void broadcastPause() {
+		List<TClient> clients = getClients();
+
+		long time = Date.getTime();
+
+		for (TClient t : clients) {
+			t.pause(time);
+		}
+	}
+
+	public final void broadcastSync(long realTime, long clipTime) {
+		List<TClient> clients = getClients();
+
+		for (TClient t : clients) {
+			t.sync(realTime, clipTime);
+		}
+	}
+
+	public abstract List<TClient> getClients();
+}
