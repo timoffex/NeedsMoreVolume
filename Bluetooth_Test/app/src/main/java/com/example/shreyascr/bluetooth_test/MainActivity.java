fa2509ef7deb02771fa42ac54bff3fb67ec31a38
Soham Saha <sohamjs2@gmail.com>
2015-01-31 22:28:11 -0800

    MORE STUFF ADDED
    i added more stuff

--- Bluetooth_Test/app/src/main/java/com/example/shreyascr/bluetooth_test/MainActivity.java
+++ Bluetooth_Test/app/src/main/java/com/example/shreyascr/bluetooth_test/MainActivity.java
@@ -1,39 +1,88 @@
 package com.example.shreyascr.bluetooth_test;
 
+import java.util.Set;
 import android.support.v7.app.ActionBarActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.button
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
+    ArrayAdapter<String>
+	Button connectNew;
+	ListView listView;
+	BluetoothAdapter btAdapter;
+	Set<BluetoothDevices> devicesArray;
+	ArrayList<String> pairedDevices;
+	IntentFilter filter;
+	BroadcastReceiver receiver;
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+		init();
+		if (btAdapter == null){
+			Toast.makeText(getApplicationContext(), "No bluetooth detecte", 0).show();
+			finish();
+		}
+		else{
+			if (!bt.Adapter.isEnabled()){
+				Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityforResult(intent, 1);
+			}
+			getPairedDevices();
+		}
+	}
+	
+	private void getPairedDevices(){
+		devicesArray = btAdapter.getBondedDevices();
+		if(devicesArray.size() > 0){
+			for(BluetoothDevice device:devicesArray){
+				pairedDevices.add(device.getName();
+			}
+		}
+	}
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+	private void init(){
+		connectNew=(Button)findViewById(R.id.bConnectNew);
+		listView=(ListView)findViewById(R.id.ListView);
+		listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,0);
+		listView.setAdapter(listAdapter);
+		btAdapter = BluetoothAdapter.getDefaultAdapter();
+		pairedDevices = new ArrayList<String>();
+		filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		receive = new BroadcastReceiver(){
+			@Override
+			public void onReceive (Context context, Intent intent){
+				String action = intent.getAction();
+				
+				if (BluetoothDevice.ACTION_FOUND.equals(action)){
+					BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVIC);
+					listAdapter.add(device.getName() + "\n" + device.getAddress());
+				}
+			}
+		};
+		registerReceiver(receiver, filter);
+		IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+		registerReceiver(receiver, filter);
+		IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+		registerReceiver(receiver, filter);
+		IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+		registerReceiver(receiver, filter);
+	}
+	
+	protected void onActivityResult (int requestCode, int resultCode, Intent data){
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == RESULT_CANCLED){
+			Toast.makeText(getAplicationContext(), "Bluetooth must be enabled to continue");
+			finsh();
+		}
+	}		
 }
