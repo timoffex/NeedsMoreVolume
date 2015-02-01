892e471744e74e9e5eb4f1be62d813428430bf79
timoffex <timoffex@gmail.com>
2015-01-31 23:21:21 -0800

    Added code to enable Bluetooth

--- Test_UI/app/src/main/java/com/example/shreyascr/test_ui/MainMenu.java
+++ Test_UI/app/src/main/java/com/example/shreyascr/test_ui/MainMenu.java
@@ -1,6 +1,7 @@
 package com.example.shreyascr.test_ui;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -23,6 +24,15 @@ public class MainMenu extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        // Get and enable Bluetooth
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter == null) {
+            // Bluetooth not supported; show error message
+        }
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
 
         interfaceMain = new AndroidInterfaceMain(btAdapter);
     }
