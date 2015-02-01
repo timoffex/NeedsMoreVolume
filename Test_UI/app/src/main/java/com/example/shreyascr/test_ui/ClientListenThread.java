10ee12d7bed9bf7f60fbffc22d86242355e669e7
timoffex <timoffex@gmail.com>
2015-01-31 23:43:04 -0800

    Started ClientListenThread.java

---
+++ Test_UI/app/src/main/java/com/example/shreyascr/test_ui/ClientListenThread.java
@@ -0,0 +1,43 @@
+package com.example.shreyascr.test_ui;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+/**
+ * Created by timoffex on 1/31/15.
+ */
+public class ClientListenThread extends Thread {
+    private BluetoothAdapter adapter;
+    private BluetoothDevice device;
+    private BluetoothSocket server;
+
+    public ClientListenThread(BluetoothAdapter a, BluetoothDevice d) throws IOException {
+        adapter = a;
+        device = d;
+
+        server = device.createRfcommSocketToServiceRecord(UUID.fromString("NeedsMoreVolume"));
+    }
+
+    public void run() {
+        InputStream in;
+        try {
+            in = server.getInputStream();
+        } catch (IOException e) { return; }
+
+        byte[] bytes = new byte[1024];
+        try {
+            while (in.read(bytes) != -1) {
+                process(bytes);
+            }
+        } catch (IOException e) { }
+    }
+
+    public void cancel() {
+
+    }
+}
