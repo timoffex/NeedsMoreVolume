81defdf290cf6282c2654dc7553f51f160edc47d
Shreyas Canchi Radhakrishna <shreyas.rad@gmail.com>
2015-01-31 18:16:41 -0800

    Added Test_UI

---
+++ Test_UI/app/src/main/java/com/example/shreyascr/test_ui/MainMenu.java
@@ -0,0 +1,22 @@
+package com.example.shreyascr.test_ui;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+
+public class MainMenu extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void sendHost(View view) {
+        //
+    }
+    public void sendJoin(View view) {
+        //
+    }
+}
